df1a05a557a3fc4f3c7854538ad4aec5214ee9f1
Sergei Stadnik <sergeisqa@gmail.com>
2019-04-18 11:46:51 -0700

    Smart contract

---
+++ README.md
@@ -0,0 +1,54 @@
+Testing task
+------------
+Create a smart contract named `Splitter` whereby:
+
+- [x] there are 3 people: Alice, Bob and Carol.
+- [x] we should see the balance of the Splitter contract on the `Web page`.
+- [x] whenever Alice sends ether to the contract for it to be split, half of 
+it goes to Bob and the other half to Carol.
+- [x] we can see the balances of Alice, Bob and Carol on the Web page.
+- [x] Alice can use the Web page to split her ether.
+
+It would be even better if you could team up with different people 
+impersonating Alice, Bob and Carol, all cooperating on a test net.
+
+# Prerequisites
+
+1. Users:  (A)lice, (B)ob and (C)arol
+2. When `A` sends ETH to `B` and `C`, it splits 50/50
+3. `A` is able to split ETH on the website
+4. We are able to see total balance
+5. We are able to see separate balance of `A`, `B` and `C`.
+
+## Setting up your environment
+
+* Check if Node.js and truffle are installed by typing in your 
+terminal: 
+    * `node -v` 
+    * and then `truffle version`
+* Initialize truffle `truffle init`
+* Create package.json file `npm init`
+* Install OpenZeppelin library `npm install openzeppelin-solidity`
+* Create .sol file in the contracts folder.
+* Import from OpenZeppelin libraries using import keyword. 
+    * Please note the importing format is **using forward slashes**. 
+    * If import is highlighted red in your editor
+        * add following to your user settings(e.g. in VSC):
+`"solidity.packageDefaultDependenciesContractsDirectory": ""`,
+`"solidity.packageDefaultDependenciesDirectory": "node_modules"`, 
+* Modify `truffle-config.js` file (`truffle.js` for Mac). 
+* Run `truffle compile`. 
+    * Please check the required version of the Solidity compiler (for all .sol files including imported libraries) 
+    * by running `truffle version`. 
+    * If needed, run `npm uninstall -g truffle` 
+    * and the `npm install -g truffle`
+* Create `2_deploy_token.js` in the migrations folder.
+* Run the blockchain emulator. 
+    * Install `npm install ganache-cli` 
+    * and run it `ganache-cli`.
+* Run `truffle migrate --reset` to migrate the contract.
+
+
+# Tip's
+- [event](https://stackoverflow.com/questions/35545625/event-triggering-in-solidity) 
+
---
+++ contracts/Splitter.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.5.0;
+
+import {Toggled} from "./Toggled.sol";  // Toggled is Owned
+
+contract Splitter is Toggled {
+
+    // keep/save balance for B and C 'key':'value'
+    // address: recipient
+    // uint: amount
+    mapping(address => uint) private balances;
+
+    // When A sends Eth - tokens to B and C, it splits 50/50
+    // Sample event definition: use 'event' keyword and define the parameters
+    event LogEthSplitted(
+        address indexed from, // msg.sender is sending - 100 ETH
+        address indexed Bob, // recipient B is getting - 50 ETH
+        address indexed Alice, // recipient C is getting - 50 ETH
+        uint initialAmount // total amount = 100 ETH
+    );
+
+    // Withdrawal Eth
+    event LogEthWithdrawal(
+        address indexed to, // send Eth to B,C
+        uint amount // total amount = 100 Eth
+    );
+
+    function splitEth(address Bob, address Alice) public payable isActive {
+        require(Bob != address(0), "Address of 'Bob' cannot be empty");
+        // Check if address of Bob is NOT empty
+        require(Alice != address(0), "Address of 'Alice' cannot be empty");
+        // Check if address of Alice is NOT empty
+        require(msg.value != 0, "Message value cannot be 0");
+        // compare money of owner which was sent
+        require((msg.value % 2) == 0, "Message value must be even");
+        // Check if our amount can be split in two the same parts
+        uint amount = msg.value / 2;
+        // Divide amount on 2 parts
+        balances[Bob] += amount;
+        // Check how much money Bob has right now = 0[Bob]=50+50
+        balances[Alice] += amount;
+        // 0[Alice]=50+50
+        emit LogEthSplitted(msg.sender, Bob, Alice, msg.value);
+        // log data
+        // emit LogEthSplitted(from, Bob, Alice, initialAmount); // emit получатель
+    }
+
+    // 3 states 1:money in contract, 2:money in msg.sender, 3:send money to B,C
+    function withdraw() public {
+        // Owned has balance = amount from contract 'initialAmount'
+        uint amount = balances[msg.sender];
+        // ?????
+        balances[msg.sender] = 0;
+        // if (amount > 0) then go next line
+        require(amount > 0, "No balance available");
+        // error message 'No balance available' if the amount = 0
+        // Transfer money to B,C
+        msg.sender.transfer(amount);
+        // msg.sender created of contract
+        // Call the event that will fire at browser (client-side)
+        emit LogEthWithdrawal(msg.sender, amount);
+    }
+
+    function() external {
+        revert();
+    }
+
+}
